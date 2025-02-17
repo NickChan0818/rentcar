cbfab505cd2621b4647dd6bfa7010c63dc410162
NickChan-Pentium <atohsiang@gmail.com>
2025-02-17 22:48:11 +0900

    init scooter data in db when bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DataSource } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { getDataSourceToken } from '@nestjs/typeorm';
 import { JwtService } from '@nestjs/jwt';
 import { AuthGuard } from './auth/auth.guard';
+import { Scooter } from './scooter/entities/scooter.entity';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,8 +15,15 @@ async function bootstrap() {
   // 檢查數據庫連接狀態
   const dataSource = app.get<DataSource>(getDataSourceToken());
   if (dataSource.isInitialized) {
-    console.log('Database connection established successfully');
     console.log('pgweb: http://localhost:8081');
+
+    // scooter檢查並插入資料
+    const scooterRepository: Repository<Scooter> = dataSource.getRepository(Scooter);
+    const scooterCount = await scooterRepository.count();
+    if (scooterCount === 0) {
+      const scooters = [{ isRenting: false }, { isRenting: false }, { isRenting: false }];
+      await scooterRepository.save(scooters);
+    }
   } else {
     console.error('Failed to connect to the database');
   }
